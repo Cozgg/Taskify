ddbf8d544b43bf92541e433a10061ec2860c49d2
Cozg <nguyenhuucong295@gmail.com>
2026-04-16 10:31:39 +0700

    fix BoardController and SecurityCustomLogic

--- backend/Taskify/src/main/java/com/ccq/utils/SecurityCustomLogic.java
+++ backend/Taskify/src/main/java/com/ccq/utils/SecurityCustomLogic.java
@@ -1,23 +1,14 @@
 package com.ccq.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.ccq.repository.BoardRepository;
 import com.ccq.repository.CardRepository;
 import com.ccq.repository.CommentRepository;
 import com.ccq.repository.ListRepository;
 import com.ccq.repository.UserRepository;
-import com.ccq.repository.UserWorkspaceRepository;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.ccq.utils;
-
-import com.ccq.repository.CardRepository;
-import com.ccq.repository.CommentRepository;
 import com.ccq.repository.WorkspaceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -42,11 +33,7 @@ public class SecurityCustomLogic {
     private UserRepository userRepo;
 
     @Autowired
-    private UserWorkspaceRepository userWorkspaceRepo;
-
-//    public boolean isCommentOwner(int commentId, String username) {
-//        return this.commRepo.isCommentOwner(commentId, username);
-//    }
+    private WorkspaceRepository workspaceRepo;
 
     public boolean canAccessBoard(String username, int boardId) {
         Integer wsId = resolveWorkspaceId("BOARD", boardId);
@@ -70,7 +57,7 @@ public class SecurityCustomLogic {
             case "LIST":
                 return listRepo.getById(id).getBoardId().getWorkspaceId().getId();
             case "CARD":
-                return cardRepo.findCardById(id).getBoardList().getBoardId().getWorkspaceId().getId();
+                return cardRepo.findCardById(id).getListId().getBoardId().getWorkspaceId().getId();
             default:
                 throw new IllegalArgumentException("Unknown type: " + type);
         }
@@ -84,29 +71,23 @@ public class SecurityCustomLogic {
         if (user == null) {
             return false;
         }
-        return userWorkspaceRepo.isUserInWorkspace(user.getId(), workspaceId);
-    @Autowired
-    private CommentRepository commRepo;
-    
-    @Autowired
-    private CardRepository cardRepo;
-    
-    @Autowired
-    private WorkspaceRepository workspaceRepo;
-    
-    public boolean isCommentOwner(int commentId, String username){
+        var members = workspaceRepo.getMembersByWorkspaceId(workspaceId);
+        return members.stream().anyMatch(u -> u.getId().equals(user.getId()));
+    }
+
+    public boolean isCommentOwner(int commentId, String username) {
         return this.commRepo.isCommentOwner(commentId, username);
     }
-    
-    public boolean isWorkspaceAdminOfThisComment(int commentId, String username){
+
+    public boolean isWorkspaceAdminOfThisComment(int commentId, String username) {
         return this.commRepo.isWorkspaceAdminOfThisComment(commentId, username);
     }
-    
-    public boolean isWorkspaceAdminOfThisCard(int cardId, String username){
+
+    public boolean isWorkspaceAdminOfThisCard(int cardId, String username) {
         return this.cardRepo.isWorkspaceAdminOfThisCard(cardId, username);
     }
-    
-    public boolean isAdminOfThisWorkspace(int workspaceId, String username){
+
+    public boolean isAdminOfThisWorkspace(int workspaceId, String username) {
         return this.workspaceRepo.isAdminOfThisWorkspace(workspaceId, username);
     }
 }
