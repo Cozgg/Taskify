08c023cd21b1d06b41e2724aa7fabf644e63fd40
Cozg <nguyenhuucong295@gmail.com>
2026-04-22 10:56:56 +0700

    Cập nhật cấu hình và gitignore

--- backend/Taskify/src/main/java/com/ccq/configs/WebAppContextConfigs.java
+++ backend/Taskify/src/main/java/com/ccq/configs/WebAppContextConfigs.java
@@ -6,7 +6,9 @@ package com.ccq.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -19,11 +21,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     "com.ccq.repository",
     "com.ccq.service"
 })
-public class WebAppContextConfigs implements WebMvcConfigurer{
+public class WebAppContextConfigs implements WebMvcConfigurer {
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
-    
+
+    // http://localhost:8080/swagger-ui/index.html
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/")
+                .resourceChain(false);
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Áp dụng cho mọi URL
+                .allowedOrigins("http://localhost:3000", "http://localhost:3002") // Chỉ cho phép React gọi
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
